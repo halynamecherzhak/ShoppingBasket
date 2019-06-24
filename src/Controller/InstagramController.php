9326754b21208ef4c5a677a6e45a628a8c38b355
Halyna Mecherzhak <Halyna_Mecherzhak@epam.com>
2019-06-24 15:58:44 +0300

    added possibility to extract instagram photos by access_token

--- src/Controller/InstagramController.php
+++ src/Controller/InstagramController.php
@@ -8,10 +8,10 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\Mapping\ReflectionEmbeddedProperty;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\Exception\RedirectionException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Flex\Response;
 
 class InstagramController extends  AbstractController
 {
@@ -22,47 +22,12 @@ class InstagramController extends  AbstractController
         );
 
     /**
-     * @Route("/loginInstagram", name="loginInstagram")
-     */
-    public function loginInstagram(){
-        return $this->render('security/login.php');
-    }
-
-    /**
      * @Route("/getLoginURL")
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public  function  getLoginURL(){
          return $this->redirect( "https://api.instagram.com/oauth/authorize/?client_id=".$this->settings['clientID']."&redirect_uri=".$this->settings['redirectURI']."&response_type=code");
-         //return $this->redirect("https://api.instagram.com/oauth/authorize/?client_id=".$this->settings['clientID']."&redirect_uri=".$this->settings['redirectURI']."&response_type=token");
     }
-
-//    /**
-//     * @return string
-//     * @Route("/callback")
-//     */
-//    public function callback(){
-//        $code = $_GET['code'];
-//        $postFileds = array(
-//            "client_id" => $this->settings['clientID'],
-//            "client_secret" => $this->settings['clientSecret'],
-//            "grant_type" => "authorization_code",
-//            "redirect_uri" => $this->settings['redirectURI'],
-//            "code" => $code
-//        );
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFileds);
-//        $response = curl_exec($ch);
-//
-//        curl_close($ch);
-//        $data = json_decode($response, true);
-//
-//        $accessToken = $data['access_token'];
-//        var_dump($accessToken);
-//
-//        return new Response("<pre>");
-//    }
 
     public  function  getAccessTokenAndUserDetails($code){
         $postFileds = array(
@@ -89,13 +54,40 @@ class InstagramController extends  AbstractController
      * @Route("/callback")
      */
     public function callback(){
-        if(isset($_GET['error'])){
-            header('Location: security/login.php');
-        }
         $data = $this->getAccessTokenAndUserDetails($_GET['code']);
-        //dd($data);
-        echo "<pre>";
-        //return  new Response("<pre>".var_dump($data));
-        return var_dump($data);
+
+        return $this->redirectToRoute('getPosts' , ['access_token' => $data['access_token']]);
+    }
+
+    /**
+     * @Route("/getPosts", name="getPosts")
+     */
+    public  function  getData(){
+        $access_token=$_GET['access_token'];
+        $photo_count=5;
+        $json_link="https://api.instagram.com/v1/users/self/media/recent/?";
+        $json_link.="access_token={$access_token}&count={$photo_count}";
+        $json = file_get_contents($json_link);
+        $obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
+
+       foreach ($obj['data'] as $post) {
+
+            $pic_text = $post['caption']['text'];
+            $pic_link = $post['link'];
+            $pic_src = str_replace("http://", "https://", $post['images']['standard_resolution']['url']);
+
+            echo "<div class='col-md-4 col-sm-6 col-xs-12 item_box'>";
+           echo "<a href='{$pic_link}' target='_blank'>";
+            echo "<img class='img-responsive photo-thumb' src='{$pic_src}' alt='{$pic_text}'>";
+            echo "</a>";
+            echo "<p>";
+            echo "<p>";
+            echo "</div>";
+            echo "</p>";
+            echo "<p>{$pic_text}</p>";
+            echo "</p>";
+            echo "</div>";
+        }
+        return new Response();
     }
 }
